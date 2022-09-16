5403f122e5eef5ed34c0131335af6e025cf14265
Carlos Pavajeau <cantte098@gmail.com>
2022-09-15 19:57:30 -0500

    feat(category): add search all

---
+++ src/Million/Application/SearchAll/SearchAllCategoriesQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Million.Application.SearchAll;
+
+public record SearchAllCategoriesQuery : IRequest<IEnumerable<CategoryResponse>>;
---
+++ src/Million/Application/SearchAll/SearchAllCategoriesQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Million.Domain;
+
+namespace Million.Application.SearchAll;
+
+public class SearchAllCategoriesQueryHandler : IRequestHandler<SearchAllCategoriesQuery, IEnumerable<CategoryResponse>>
+{
+    private readonly DbContext _context;
+
+    public SearchAllCategoriesQueryHandler(DbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<CategoryResponse>> Handle(SearchAllCategoriesQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Set<Category>()
+            .Select(x => new CategoryResponse(x.Name, x.Difficulty))
+            .ToListAsync(cancellationToken);
+    }
+}
