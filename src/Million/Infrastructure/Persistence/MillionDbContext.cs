6315846895452f6d54de639cc1b8cdc3e3149b9f
Carlos Pavajeau <cantte098@gmail.com>
2022-09-15 19:55:40 -0500

    feat: add DbContext

---
+++ src/Million/Infrastructure/Persistence/MillionDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Million.Domain;
+
+namespace Million.Infrastructure.Persistence;
+
+public class MillionDbContext : DbContext
+{
+    public MillionDbContext(DbContextOptions<MillionDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; } = default!;
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MillionDbContext).Assembly);
+    }
+}
