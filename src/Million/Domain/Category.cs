615103daf99ebe2b94f02cea8f17c7f1a358ba5d
Carlos Pavajeau <cantte098@gmail.com>
2022-09-15 19:55:06 -0500

    feat(category): add domain entity

---
+++ src/Million/Domain/Category.cs
@@ -0,0 +1,23 @@
+﻿namespace Million.Domain;
+
+public class Category
+{
+    private Category()
+    {
+        // Ef Core
+    }
+
+    [Key] public int Id { get; set; }
+
+
+    [Required] [MaxLength(50)] public string Name { get; set; }
+    [Required] public int Difficulty { get; set; }
+
+    public static Category Create(int difficulty)
+    {
+        return new Category
+        {
+            Difficulty = difficulty
+        };
+    }
+}
