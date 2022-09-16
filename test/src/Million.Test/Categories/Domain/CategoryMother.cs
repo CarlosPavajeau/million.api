d1fbf7b6c7f187f02e5b80f6a3bee7ea0841d01a
Carlos Pavajeau <cantte098@gmail.com>
2022-09-16 09:14:39 -0500

    test(categories): add search all unit tests

---
+++ test/src/Million.Test/Categories/Domain/CategoryMother.cs
@@ -0,0 +1,12 @@
+﻿using Million.Categories.Domain;
+
+namespace Million.Test.Categories.Domain;
+
+public static class CategoryMother
+{
+    public static Category Random()
+    {
+        var faker = new Faker();
+        return Category.Create(faker.Random.Word(), faker.Random.Number(1, 10));
+    }
+}
