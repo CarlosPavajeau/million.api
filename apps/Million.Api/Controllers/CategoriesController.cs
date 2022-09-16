3e806e8ec56cacffae1aea1fd5a118adc7b29122
Carlos Pavajeau <cantte098@gmail.com>
2022-09-15 19:59:29 -0500

    feat(categories): add get all endpoint

---
+++ apps/Million.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Million.Application.SearchAll;
+
+namespace Million.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    
+    public CategoriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var categories = await _mediator.Send(new SearchAllCategoriesQuery());
+
+        return Ok(categories);
+    }
+}
