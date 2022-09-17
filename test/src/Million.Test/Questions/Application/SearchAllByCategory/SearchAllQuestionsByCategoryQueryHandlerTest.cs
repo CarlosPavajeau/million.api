69dbe9ee8404c3f30d5656f16c0b7e01cd0a7603
Carlos Pavajeau <cantte098@gmail.com>
2022-09-16 22:48:20 -0500

    test(questions): add search all by category unit tests

---
+++ test/src/Million.Test/Questions/Application/SearchAllByCategory/SearchAllQuestionsByCategoryQueryHandlerTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Million.Questions.Application.SearchAllByCategory;
+using Million.Shared.Infrastructure.Persistence;
+using Million.Test.Questions.Domain;
+
+namespace Million.Test.Questions.Application.SearchAllByCategory;
+
+public class SearchAllQuestionsByCategoryQueryHandlerTest
+{
+    private readonly SearchAllQuestionsByCategoryQueryHandler _handler;
+    private readonly MillionDbContext _context;
+
+    public SearchAllQuestionsByCategoryQueryHandlerTest()
+    {
+        var dbContextOptions = new DbContextOptionsBuilder<MillionDbContext>()
+            .UseInMemoryDatabase("test")
+            .Options;
+        _context = new MillionDbContext(dbContextOptions);
+
+        _handler = new SearchAllQuestionsByCategoryQueryHandler(_context);
+    }
+
+    [Fact]
+    public async Task ShouldReturnAllQuestionsByCategory()
+    {
+        // Arrange
+        var question = QuestionMother.Random(1);
+        _context.Questions.Add(question);
+        await _context.SaveChangesAsync();
+
+        // Act
+        var query = new SearchAllQuestionsByCategoryQuery(1);
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull()
+            .And.HaveCount(1);
+        
+        // Arrange
+        _context.Questions.Add(QuestionMother.Random(2));
+        _context.Questions.Add(QuestionMother.Random(2));
+        await _context.SaveChangesAsync();
+        
+        // Act
+        result = await _handler.Handle(query, CancellationToken.None);
+        
+        // Assert
+        result.Should().NotBeNull()
+            .And.HaveCount(1);
+        
+        // Act
+        query = new SearchAllQuestionsByCategoryQuery(2);
+        result = await _handler.Handle(query, CancellationToken.None);
+        
+        // Assert
+        result.Should().NotBeNull()
+            .And.HaveCount(2);
+    }
+}
---
+++ test/src/Million.Test/Questions/Domain/QuestionMother.cs
@@ -0,0 +1,13 @@
+﻿using Million.Questions.Domain;
+
+namespace Million.Test.Questions.Domain;
+
+public static class QuestionMother
+{
+    public static Question Random(int categoryId)
+    {
+        var faker = new Faker();
+
+        return Question.Create(faker.Random.Words(10), faker.Random.Float(1, 100), categoryId);
+    }
+}
