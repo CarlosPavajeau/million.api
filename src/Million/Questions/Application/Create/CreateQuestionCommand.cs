f2a930cf9b41e0a5cc46cc6318b8954a9fbf499e
Carlos Pavajeau <cantte098@gmail.com>
2022-09-17 21:26:38 -0500

    feat(questions): add create support

---
+++ src/Million/Questions/Application/Create/CreateQuestionCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Million.Questions.Application.Create;
+
+public record CreateAnswerCommand(string Content, bool IsCorrect);
+
+public record CreateQuestionCommand
+    (string Content, float Reward, ICollection<CreateAnswerCommand> Answers) : IRequest<int>
+{
+    public int CategoryId { get; set; }
+}
---
+++ src/Million/Questions/Application/Create/CreateQuestionCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Mapster;
+using Million.Questions.Domain;
+
+namespace Million.Questions.Application.Create;
+
+public class CreateQuestionCommandHandler : IRequestHandler<CreateQuestionCommand, int>
+{
+    private readonly DbContext _context;
+
+    public CreateQuestionCommandHandler(DbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(CreateQuestionCommand request, CancellationToken cancellationToken)
+    {
+        if (request.Answers.Count > 4)
+        {
+            throw new InvalidOperationException("A question can have more than 4 answers");
+        }
+
+        var question = request.Adapt<Question>();
+        foreach (var answer in question.Answers)
+        {
+            answer.Question = question;
+        }
+
+        await _context.AddAsync(question, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return question.Id;
+    }
+}
